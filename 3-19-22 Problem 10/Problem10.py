f0d97660c5c68bdedf80504b6a4e814bcfb405e4
dunnm10atwit <71147464+dunnm10atwit@users.noreply.github.com>
2022-03-19 15:01:46 -0400

    problem10Sol

---
+++ 3-19-22 Problem 10/Problem10.py
@@ -0,0 +1,9 @@
+import time
+
+def scheduler(f,n):
+    time.sleep(n)
+    f()
+
+
+#Looked up answer @ https://sathishbabu96n.medium.com/daily-coding-problem-problem-10-da50b93bfc67
+#This is not the best solution, just the simplest to implement
