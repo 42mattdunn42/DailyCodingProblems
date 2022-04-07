36b2912ae9769e877ca9d34b7f6ce318e9464038
dunnm10atwit <71147464+dunnm10atwit@users.noreply.github.com>
2022-04-07 17:04:00 -0400

    problem29semisolution

--- 4-7-22 Problem 29/Problem29.java
+++ 4-7-22 Problem 29/Problem29.java
@@ -1,7 +1,21 @@
 public class Problem29{
 
     public static String runLengthEncoder(String s){
-        return "";
+        String out = "";
+        //Will not work if there are more than 9 of a single digit
+        char prev = s.charAt(0);
+        int count = 1;
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == prev){
+                count++;
+            }else{
+                out += Integer.toString(count) + prev;
+                count = 1;
+            }
+            prev = s.charAt(i);
+        }
+        out += Integer.toString(count) + prev;
+        return out;
     }
 
     public static String runLengthDecoder(String s){
@@ -21,5 +35,11 @@ public class Problem29{
     public static void main(String args[]){
         String test1 = "4A3B2C1D2A";
         System.out.println(runLengthDecoder(test1));
+        String test2 = "AAAABBBCCDAA";
+        System.out.println(runLengthEncoder(test2));
+        String test3 = "aabcddddddopnnnnfffw";
+        String temp = runLengthEncoder(test3);
+        System.out.println(temp);
+        System.out.println(runLengthDecoder(temp));
     }
 }
